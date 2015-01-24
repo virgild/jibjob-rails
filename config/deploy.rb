efeab3daf267a38b31daf9b09118ddff12008b1a
Virgil Dimaguila <virgild@gmail.com>
2015-01-24 00:39:38 -0500

    Capify this.

---
+++ config/deploy.rb
@@ -0,0 +1,51 @@
+# config valid only for current version of Capistrano
+lock '3.3.5'
+
+set :application, 'jibjob'
+set :repo_url, 'git@bitbucket.org:virgil/jibjob.git'
+
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/apps/jibjob'
+
+# Default value for :scm is :git
+set :scm, :git
+
+# Default value for :format is :pretty
+set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
+# Default value for linked_dirs is []
+set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# Default value for default_env is {}
+set :default_env, { path: "/opt/ruby/2.2.0/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+set :sidekiq_role, :worker
+set :sidekiq_queue, []
+
+namespace :deploy do
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+end
