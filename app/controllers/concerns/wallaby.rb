b65605f66c635382204d446013af7d7c37b316a4
Virgil Dimaguila <virgild@gmail.com>
2015-01-26 19:00:27 -0500

    Wallaby.

--- app/controllers/concerns/wallaby.rb
+++ app/controllers/concerns/wallaby.rb
@@ -2,21 +2,24 @@ module Wallaby
   extend ActiveSupport::Concern
 
   included do
-    before_filter :check_wallaby_pass, except: [:wallaby, :features]
+    before_filter :set_wallaby_pass
+    before_filter :check_wallaby_pass
   end
 
   private
 
   def set_wallaby_pass
-    cookies[:wallaby] ||= {
-      value: 'pass',
-      expires: 1.day.from_now
-    }
+    if params[:_wallaby]
+      cookies.signed[:wallaby] ||= {
+        value: 'pass',
+        expires: 1.day.from_now
+      }
+    end
   end
 
   def check_wallaby_pass
-    if cookies[:wallaby] != 'pass'
-      redirect_to wallaby_url
+    if cookies.signed[:wallaby] != 'pass'
+      render controller: :front, action: :wallaby, layout: nil
     end
   end
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -35,7 +35,7 @@ set :default_env, { path: "/opt/ruby/2.2.0/bin:$PATH" }
 # set :keep_releases, 5
 
 set :sidekiq_role, :worker
-set :sidekiq_queue, []
+set :sidekiq_queue, ['default', 'logging']
 
 namespace :deploy do
 
