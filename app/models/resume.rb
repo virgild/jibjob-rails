b423a81d096447f8c433cedcd96c9d8c62f4701a
Virgil Dimaguila <virgild@gmail.com>
2015-02-14 22:05:19 -0500

    Scoped routes.

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -35,6 +35,9 @@ class Resume < ActiveRecord::Base
   validates_uniqueness_of :name, scope: :user
   validates_uniqueness_of :slug
 
+  validates_format_of :slug, with: /\A[A-Za-z0-9-]+\Z/
+  validates_exclusion_of :slug, in: ['app', 'pages', 'admin', 'tmp', 'public', 'support', 'help', 'rails', 'api']
+
   belongs_to :user
   has_many :publication_views
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,40 +3,45 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web => "/__kickside"
 
-  resources :users, param: :username, except: [:index, :destroy] do
-    resources :resumes do
-      member do
-        get "delete"
-        get "stats" => "resume_stats#index"
+  scope '/app' do
+    resources :users, param: :username, except: [:index, :destroy] do
+      resources :resumes do
+        member do
+          get "delete"
+          get "stats" => "resume_stats#index"
+        end
       end
     end
-  end
 
-  get '/app/confirm' => "signup_confirmations#update"
+    get '/app/confirm' => "signup_confirmations#update"
 
-  resources :sessions, only: [:new, :create] do
-    collection do
-      get "logout"
+    resources :sessions, only: [:new, :create] do
+      collection do
+        get "logout"
+      end
+    end
+
+    namespace :admin do
+      resources :dashboards, only: [:index]
+      resources :users, param: :username
+      root to: "dashboards#index"
     end
   end
 
-  get "/pub/:slug" => "publications#show", as: "publication"
+  scope '/pages' do
+    resources :support, only: [:index]
 
-  resources :support, only: [:index]
-
-  # Static pages
-  get "features" => "front#features"
-  get "get_started" => "front#get_started"
-  get "pricing" => "front#pricing"
-  get "terms_of_service" => "front#terms_of_service"
-  get "privacy_policy" => "front#privacy_policy"
-  get "wallaby" => "front#wallaby"
-
-  namespace :admin do
-    resources :dashboards, only: [:index]
-    resources :users, param: :username
-    root to: "dashboards#index"
+    # Static pages
+    get "features" => "front#features"
+    get "get_started" => "front#get_started"
+    get "pricing" => "front#pricing"
+    get "terms_of_service" => "front#terms_of_service"
+    get "privacy_policy" => "front#privacy_policy"
+    get "wallaby" => "front#wallaby"
   end
+
+
+  get "/:slug" => "publications#show", as: "publication", slug: /[A-Za-z0-9-]+/
 
   root "front#index"
 
