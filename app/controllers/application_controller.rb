85453de42222670c8cb1abb5ed2d9ee6c724b8de
Virgil Dimaguila <virgild@gmail.com>
2015-02-23 12:06:35 -0500

    Disable wallaby.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  include Wallaby
   include AuthenticatedUser
   include GeokitOverrides
 end
