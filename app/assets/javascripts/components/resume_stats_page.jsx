3d1db8be7a580bd637b7db0a3a5e00189d2a47cd
Virgil Dimaguila <virgild@gmail.com>
2015-03-02 19:20:42 -0500

    Better date and location format for page view item.

--- app/assets/javascripts/components/resume_stats_page.jsx
+++ app/assets/javascripts/components/resume_stats_page.jsx
@@ -184,22 +184,16 @@ var PublicationViewListItem = React.createClass({
   render: function() {
     var data = this.props.viewData;
 
-    var location = (function(city, state, country) {
-      return (
-        <div className="col-sm-9">
-          <span className="field-label">Location:</span> <span className="field-value">{city}, {state}, {country}</span>
-        </div>
-      );
-    }(data.city, data.state, data.country));
-
     return (
       <div className="pubview row">
         <div className="col-sm-6"><span className="field-label">Date:</span> <span className="field-value">{data.created_at}</span></div>
-        <div className="col-sm-6 ipaddr"><span className="field-label">IP:</span> <span className="field-value">{this.props.viewData.ip_addr}</span></div>
-        {location}
-        <div className="col-sm-12"><span className="field-label">URL:</span> <span className="field-value">{this.props.viewData.url}</span></div>
-        <div className="col-sm-12"><span className="field-label">Referrer:</span> <span className="field-value">{this.props.viewData.referrer}</span></div>
-        <div className="col-sm-12"><span className="field-label">User Agent:</span> <span className="field-value">{this.props.viewData.user_agent}</span></div>
+        <div className="col-sm-6 ipaddr"><span className="field-label">IP:</span> <span className="field-value">{data.ip_addr}</span></div>
+        <div className="col-sm-9">
+          <span className="field-label">Location:</span> <span className="field-value">{data.location}</span>
+        </div>
+        <div className="col-sm-12"><span className="field-label">URL:</span> <span className="field-value">{data.url}</span></div>
+        <div className="col-sm-12"><span className="field-label">Referrer:</span> <span className="field-value">{data.referrer}</span></div>
+        <div className="col-sm-12"><span className="field-label">User Agent:</span> <span className="field-value">{data.user_agent}</span></div>
       </div>
     );
   }
--- app/serializers/publication_view_serializer.rb
+++ app/serializers/publication_view_serializer.rb
@@ -1,12 +1,17 @@
 class PublicationViewSerializer < BaseSerializer
   attributes :ip_addr, :url, :referrer, :user_agent, :created_at
-  attributes :lat, :lng, :city, :state, :country
+  attributes :lat, :lng, :city, :state, :country, :location
 
   def ip_addr
     object.ip_addr.to_s
   end
 
   def created_at
-    object.created_at.in_time_zone(object.timezone)
+    object.created_at.in_time_zone(object.timezone).strftime("%B %d, %Y %l:%M %p")
+  end
+
+  def location
+    value = [city, state, country].compact.join(', ')
+    value.blank? ? "Unknown" : value
   end
 end
