c5a24cb7e2382f51304cb5df2b67d24d2c862ef8
Virgil Dimaguila <virgild@gmail.com>
2015-03-11 15:30:05 -0400

    Resume spec updates.

--- spec/requests/resumes_spec.rb
+++ spec/requests/resumes_spec.rb
@@ -14,19 +14,19 @@ RSpec.describe "Resumes", type: :request, js: true do
   end
 
   example "creation" do
-    click_link "create-button"
+    click_on "create-button"
 
     fill_in "Name", with: "A New Resume"
     expect(page).to have_field("Link Name", with: "a-new-resume")
 
     fill_in "Access Code", with: "SESAME"
-    click_link "Load Example"
+    click_on "Load Example"
     wait_for_ajax
     click_button "Save Resume"
     expect(page).to have_content("A New Resume Details")
 
     within(".topnav") do
-      click_link "My Resumes"
+      click_on "My Resumes"
     end
     within(".resume-list") do
       expect(page).to have_content("A New Resume")
@@ -34,17 +34,21 @@ RSpec.describe "Resumes", type: :request, js: true do
   end
 
   example "edit and update" do
-    click_link "Test Resume"
-    click_link "Edit"
+    click_on "Test Resume"
+    click_on "Edit"
     expect(page).to have_css(".resume-form")
 
     fill_in "Name", with: "Modified Resume"
     expect(page).to have_field("Link Name", with: "modified-resume")
 
     fill_in "Link Name", with: "test-resume-2"
-    check("Publish now")
+    check "Publish now"
     fill_in "Access Code", with: "DOODLES"
-    click_button "Save Resume"
+
+    click_on "Save Resume"
+    wait_for_ajax
+    # TODO: Do not sleep
+    sleep 1
 
     click_on "Test Resume"
 
@@ -57,8 +61,8 @@ RSpec.describe "Resumes", type: :request, js: true do
   end
 
   example "delete" do
-    click_link "Test Resume"
-    click_link "Delete"
+    click_on "Test Resume"
+    click_on "Delete"
     expect(page).to have_text("delete this resume")
     click_button "Yes - Delete it"
 
@@ -68,15 +72,18 @@ RSpec.describe "Resumes", type: :request, js: true do
   end
 
   example "published with no access code" do
-    click_link "/test-resume"
+    click_on "/test-resume"
     expect(page).to have_text "Thomas B. Seeker"
   end
 
   example "published with access code" do
-    click_link "Test Resume"
-    click_link "Edit"
+    click_on "Test Resume"
+    click_on "Edit"
     fill_in "Access Code", with: "secret"
     click_button "Save Resume"
+    wait_for_ajax
+    # TODO: Do not sleep
+    sleep 1
     click_on "Test Resume"
 
     click_on "publish-url"
