16caad5f937dfb778eb859c76e1132cc29cf1845
Virgil Dimaguila <virgild@gmail.com>
2015-03-14 13:43:20 -0400

    Signup link helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,15 @@
 module ApplicationHelper
   include AuthenticatedUser
+
+  def facebook_auth_path
+    "/app/auth/facebook"
+  end
+
+  def twitter_auth_path
+    "/app/auth/twitter"
+  end
+
+  def google_auth_path
+    "/app/auth/google"
+  end
 end
