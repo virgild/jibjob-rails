589a38b3b14ada4e3f10eddf3438f678fced392e
Virgil Dimaguila <virgild@gmail.com>
2015-03-23 11:08:38 -0400

    Mini profiler memory store.

--- config/initializers/rack_profiler.rb
+++ config/initializers/rack_profiler.rb
@@ -2,4 +2,5 @@ if Rails.env == 'development' and ENV['MINI_PROFILER'] == '1'
   require 'rack-mini-profiler'
 
   Rack::MiniProfilerRails.initialize!(Rails.application)
+  Rack::MiniProfiler.config.storage = Rack::MiniProfiler::MemoryStore
 end
