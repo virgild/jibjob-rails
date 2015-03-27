47d5b27b762297c0ad292eb8fc7e00b762b5282e
Virgil Dimaguila <virgild@gmail.com>
2015-03-27 18:41:11 -0400

    Use https for S3 attachments.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,6 +105,7 @@ Rails.application.configure do
   Rails.configuration.x.paperclip.storage_options = {
     storage: :s3,
     s3_credentials: { bucket: ENV['S3_BUCKET_NAME'], access_key_id: ENV['AWS_ACCESS_KEY_ID'], secret_access_key: ENV['AWS_SECRET_KEY_ID'] },
+    s3_protocol: :https,
     path: -> (a) { "/resumes/:id/:style/:filename" }
   }
 end
