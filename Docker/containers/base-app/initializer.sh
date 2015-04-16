5bfc545f4337b979800db1eaa3549d23d56996a3
Virgil Dimaguila <virgild@gmail.com>
2015-04-16 16:44:57 -0400

    Initializer updates.

--- Docker/containers/base-app/initializer.sh
+++ Docker/containers/base-app/initializer.sh
@@ -1,88 +1,118 @@
 #!/bin/bash
+set -e
 
 export PATH=${RUBY_PATH}:${PATH}
 export RAILS_ENV=${RAILS_ENV:-development}
 
-mode=${MODE:-console}
+mode=${MODE:-none}
 HAS_APP=0
 
+function coloredEcho() {
+  local exp=$1;
+  local color=$2;
+  if ! [[ $color =~ '^[0-9]$' ]] ; then
+     case $(echo $color | tr '[:upper:]' '[:lower:]') in
+       black) color=0 ;;
+       red) color=1 ;;
+       green) color=2 ;;
+       yellow) color=3 ;;
+       blue) color=4 ;;
+       magenta) color=5 ;;
+       cyan) color=6 ;;
+       white|*) color=7 ;; # white or invalid color
+     esac
+  fi
+  tput setaf $color;
+  echo $exp;
+  tput sgr0;
+}
+
 # Check app directory
+coloredEcho "Checking /app directory..." cyan
 if [[ -d /app ]]; then
   if [[ -e /app/bin/rails ]]; then
     HAS_APP=1
-    echo "Found Rails app in /app"
+    coloredEcho "Found Rails app in /app." green
   else
-    echo "Cannot find Rails app in /app"
-  fi
-else
-  echo "Cannot find /app"
-fi
-
-# Prepare app
-if [[ $HAS_APP == "1" ]]; then
-  # Initialize fonts
-  echo "Copying fonts from assets and refreshing font cache..."
-  cp -R /app/app/assets/fonts/print/* /opt/fonts/truetype/
-  fc-cache -f
-
-  # Prepare gems directory
-  if [[ ! -d /vendor/bundle ]]; then
-    mkdir -p /vendor/bundle
-  fi
-  chown -R jibjob:jibjob /vendor
-
-  # Run bundler
-  if [[ $SKIP_BUNDLER == "1" ]]; then
-    echo "(Skipping bundler...)"
-  else
-    echo "Running bundler..."
-    gosu jibjob touch /vendor/BUNDLER_RUNNING
-    cd /app
-    gosu jibjob bundle install --path=/vendor/bundle
-    gosu jibjob rm -f /vendor/BUNDLER_RUNNING
-  fi
-
-  # Prepare attachments (Rails public/system) directory
-  if [[ ! -d /app/public/system ]]; then
-    echo "WARNING: /app/public/system is not mounted."
-  else
-    if [[ `stat -f -L -c %T /app/public/system` == "nfs" ]]; then
-      echo
-      echo "***********************************************"
-      echo "WARNING: /app/public/system is an NFS directory"
-      echo "***********************************************"
-      echo
-    else
-      chown -R jibjob:jibjob /app/public/system
-    fi
-  fi
-
-  # Configure nginx
-  echo "Configuring nginx with RAILS_ENV=${RAILS_ENV}..."
-  erb /config/nginx.conf.erb > /opt/nginx/conf/nginx.conf
-
-  # Invoke mode program
-  if [[ $mode == "console" ]]; then
-    echo "Entering jibjob console..."
-    exec gosu jibjob /bin/bash --login
-  elif [[ $mode == "server" ]]; then
-    echo "Starting server..."
-    exec /opt/nginx/sbin/nginx
-  elif [[ $mode == "worker" ]]; then
-    echo "Starting workers..."
-    cd /app
-    exec gosu jibjob bin/sidekiq "$@"
-  elif [[ $mode == "rails" ]]; then
-    cd /app
-    exec gosu jibjob bin/rails "$@"
-  elif [[ $mode == "su" ]]; then
-    bash -l
-  else
-    echo "Unknown MODE...doing nothing."
+    coloredEcho "Cannot find Rails app in /app." red
     exit 1
   fi
 else
-  echo "No Rails app found. Reverting to console."
-  echo "Entering jibjob console..."
-  exec gosu jibjob /bin/bash --login
+  coloredEcho "Cannot find /app. Mount the Rails root to /app in this container." red
+  exit 1
 fi
+
+# Prepare app
+
+# Initialize fonts
+function prepare_fonts()
+{
+  coloredEcho "Copying fonts from /app/app/assets/fonts/print and refreshing font cache..." cyan
+  cp -R /app/app/assets/fonts/print/* /opt/fonts/truetype/
+  fc-cache -f
+}
+
+# Prepare gems directory
+coloredEcho "Checking /vendor/bundle directory..." cyan
+if [[ ! -d /vendor/bundle ]]; then
+  coloredEcho "/vendor/bundle not found. Creating..." yellow
+  mkdir -p /vendor/bundle
+fi
+chown -R jibjob:jibjob /vendor
+
+# Run bundler
+if [[ $SKIP_BUNDLER == "1" ]]; then
+  coloredEcho "(Skipping bundler...)" yellow
+else
+  coloredEcho "Running bundler..." cyan
+  gosu jibjob touch /vendor/BUNDLER_RUNNING
+  cd /app
+  gosu jibjob bundle install --path=/vendor/bundle &> /vendor/bundle/install.log
+  gosu jibjob rm -f /vendor/BUNDLER_RUNNING
+  coloredEcho "Finished bundler install." green
+fi
+
+# Prepare attachments (Rails public/system) directory
+coloredEcho "Checking /app/public/system mount..." cyan
+if [[ ! -d /app/public/system ]]; then
+  coloredEcho "WARNING: /app/public/system is not mounted." red
+else
+  if [[ `stat -f -L -c %T /app/public/system` == "nfs" ]]; then
+    coloredEcho "WARNING: /app/public/system is an NFS directory. It may not be writable." red
+  else
+    chown -R jibjob:jibjob /app/public/system
+    coloredEcho "/app/public/system is mounted." green
+  fi
+fi
+
+# Configure nginx
+coloredEcho "Configuring nginx with RAILS_ENV=${RAILS_ENV}..." cyan
+erb /config/nginx.conf.erb > /opt/nginx/conf/nginx.conf
+coloredEcho "Written config to /opt/ntinx/conf/nginx.conf" green
+
+# Invoke mode program
+case $mode in
+  console)
+    coloredEcho "Starting jibjob shell..." cyan
+    exec gosu jibjob /bin/bash --login
+    ;;
+  server)
+    prepare_fonts
+    coloredEcho "Starting nginx..." cyan
+    exec /opt/nginx/sbin/nginx
+    ;;
+  worker)
+    prepare_fonts
+    coloredEcho "Running sidekiq..." cyan
+    cd /app
+    exec gosu jibjob bin/sidekiq "$@"
+    ;;
+  root)
+    coloredEcho "Starting root shell..." cyan
+    exec bash -l
+    ;;
+  *)
+    coloredEcho "Unknown mode...doing nothing." yellow
+    exit 1
+    ;;
+esac
