2096705c321112da5c3f8d972c2f0c366137f363
Virgil Dimaguila <virgild@gmail.com>
2015-04-17 11:39:48 -0400

    Initializer updates.

--- Docker/containers/app/initializer.sh
+++ Docker/containers/app/initializer.sh
@@ -8,18 +8,20 @@ HAS_APP=0
 PROGRAM=$1
 PROGRAM_ARGS=${*:2:500}
 
+has_missing_config_files=0
+
 # Check app directory
-echo "Checking /app directory..."
+printf "Checking /app directory..."
 if [[ -d /app ]]; then
   if [[ -e /app/bin/rails ]]; then
     HAS_APP=1
-    echo "Found Rails app in /app."
+    printf "Found Rails app in /app.\n"
   else
-    echo "Cannot find Rails app in /app."
+    printf "Cannot find Rails app in /app.\n"
     exit 1
   fi
 else
-  echo "Cannot find /app. Mount the Rails root to /app in this container."
+  printf "Cannot find /app. Mount the Rails root to /app in this container.\n"
   exit 1
 fi
 
@@ -28,53 +30,93 @@ fi
 # Initialize fonts
 function prepare_fonts()
 {
-  echo "Copying fonts from /app/app/assets/fonts/print and refreshing font cache..."
+  printf "Copying fonts from /app/app/assets/fonts/print and refreshing font cache..."
   cp -R /app/app/assets/fonts/print/* /opt/fonts/truetype/
   fc-cache -f
+  printf "done.\n"
 }
 
+function check_config_files()
+{
+  echo "Checking /app/config/ files..."
+  printf " - database.yml..."
+  if [[ -e /app/config/database.yml ]]; then
+    printf "OK\n"
+  else
+    has_missing_config_files=1
+    printf "NOT FOUND\n"
+  fi
+
+  printf " - mailer.yml..."
+  if [[ -e /app/config/mailer.yml ]]; then
+    printf "OK\n"
+  else
+    has_missing_config_files=1
+    printf "NOT FOUND\n"
+  fi
+
+  printf " - secrets.yml..."
+  if [[ -e /app/config/secrets.yml ]]; then
+    printf "OK\n"
+  else
+    has_missing_config_files=1
+    printf "NOT FOUND\n"
+  fi
+}
+
+# Check files
+check_config_files
+
 # Prepare gems directory
-echo "Checking /vendor/bundle directory..."
+printf "Checking /vendor/bundle directory..."
 if [[ ! -d /vendor/bundle ]]; then
-  echo "/vendor/bundle not found. Creating..."
+  printf "/vendor/bundle not found. Creating..."
   mkdir -p /vendor/bundle
 fi
 chown -R jibjob:jibjob /vendor
+printf "done.\n"
 
 # Run bundler
 if [[ $SKIP_BUNDLER == "1" ]]; then
-  echo "(Skipping bundler...)"
+  echo "(Skipping bundler)"
 else
-  echo "Running bundler..."
+  printf "Running bundler..."
   gosu jibjob touch /vendor/BUNDLER_RUNNING
   cd /app
-  gosu jibjob bundle install --path=/vendor/bundle &> /vendor/bundle/install.log
+  gosu jibjob bundle install --path=/vendor/bundle --no-cache --shebang=${RUBY_PATH}/ruby &> /vendor/bundle/install.log
   gosu jibjob rm -f /vendor/BUNDLER_RUNNING
-  echo "Finished bundler install."
+  printf "finished bundler install.\n"
 fi
 
 # Prepare attachments (Rails public/system) directory
 echo "Checking /app/public/system mount..."
 if [[ ! -d /app/public/system ]]; then
-  echo "WARNING: /app/public/system is not mounted."
+  echo " - WARNING: /app/public/system is not mounted."
 else
   if [[ `stat -f -L -c %T /app/public/system` == "nfs" ]]; then
-    echo "WARNING: /app/public/system is an NFS directory. It may not be writable."
+    echo " - WARNING: /app/public/system is an NFS directory. It may not be writable."
   else
     chown -R jibjob:jibjob /app/public/system
-    echo "/app/public/system is mounted."
+    echo " - /app/public/system is mounted."
   fi
 fi
 
 # Configure nginx
 echo "Configuring nginx with RAILS_ENV=${RAILS_ENV}..."
 erb /config/nginx.conf.erb > /opt/nginx/conf/nginx.conf
-echo "Written config to /opt/ntinx/conf/nginx.conf"
+echo " - Written config to /opt/ntinx/conf/nginx.conf"
+
+if [[ $has_missing_config_files -eq 1 ]]; then
+  echo
+  echo "-----------------------------------------"
+  echo "WARNING - There are missing config files."
+  echo "-----------------------------------------"
+fi
 
 # Invoke mode program
-echo
 case $PROGRAM in
   console)
+    echo
     echo "Starting jibjob shell..."
     exec gosu jibjob /bin/bash --login
     ;;
@@ -92,10 +134,12 @@ case $PROGRAM in
     exec gosu jibjob bin/sidekiq ${PROGRAM_ARGS}
     ;;
   root)
+    echo
     echo "Starting root shell..."
     exec bash -l
     ;;
   *)
+    echo
     echo "Unknown mode...doing nothing."
     exit 1
     ;;
