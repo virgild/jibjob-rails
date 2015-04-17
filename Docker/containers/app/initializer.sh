4d216495655da1a3029a6734d3fdfe98df47476f
Virgil Dimaguila <virgild@gmail.com>
2015-04-17 09:31:21 -0400

    Remove colors.

--- Docker/containers/app/initializer.sh
+++ Docker/containers/app/initializer.sh
@@ -8,38 +8,18 @@ HAS_APP=0
 PROGRAM=$1
 PROGRAM_ARGS=${*:2:500}
 
-function coloredEcho() {
-  local exp=$1;
-  local color=$2;
-  if ! [[ $color =~ '^[0-9]$' ]] ; then
-     case $(echo $color | tr '[:upper:]' '[:lower:]') in
-       black) color=0 ;;
-       red) color=1 ;;
-       green) color=2 ;;
-       yellow) color=3 ;;
-       blue) color=4 ;;
-       magenta) color=5 ;;
-       cyan) color=6 ;;
-       white|*) color=7 ;; # white or invalid color
-     esac
-  fi
-  tput setaf $color;
-  echo $exp;
-  tput sgr0;
-}
-
 # Check app directory
-coloredEcho "Checking /app directory..." cyan
+echo "Checking /app directory..."
 if [[ -d /app ]]; then
   if [[ -e /app/bin/rails ]]; then
     HAS_APP=1
-    coloredEcho "Found Rails app in /app." green
+    echo "Found Rails app in /app."
   else
-    coloredEcho "Cannot find Rails app in /app." red
+    echo "Cannot find Rails app in /app."
     exit 1
   fi
 else
-  coloredEcho "Cannot find /app. Mount the Rails root to /app in this container." red
+  echo "Cannot find /app. Mount the Rails root to /app in this container."
   exit 1
 fi
 
@@ -48,75 +28,75 @@ fi
 # Initialize fonts
 function prepare_fonts()
 {
-  coloredEcho "Copying fonts from /app/app/assets/fonts/print and refreshing font cache..." cyan
+  echo "Copying fonts from /app/app/assets/fonts/print and refreshing font cache..."
   cp -R /app/app/assets/fonts/print/* /opt/fonts/truetype/
   fc-cache -f
 }
 
 # Prepare gems directory
-coloredEcho "Checking /vendor/bundle directory..." cyan
+echo "Checking /vendor/bundle directory..."
 if [[ ! -d /vendor/bundle ]]; then
-  coloredEcho "/vendor/bundle not found. Creating..." yellow
+  echo "/vendor/bundle not found. Creating..."
   mkdir -p /vendor/bundle
 fi
 chown -R jibjob:jibjob /vendor
 
 # Run bundler
 if [[ $SKIP_BUNDLER == "1" ]]; then
-  coloredEcho "(Skipping bundler...)" yellow
+  echo "(Skipping bundler...)"
 else
-  coloredEcho "Running bundler..." cyan
+  echo "Running bundler..."
   gosu jibjob touch /vendor/BUNDLER_RUNNING
   cd /app
   gosu jibjob bundle install --path=/vendor/bundle &> /vendor/bundle/install.log
   gosu jibjob rm -f /vendor/BUNDLER_RUNNING
-  coloredEcho "Finished bundler install." green
+  echo "Finished bundler install."
 fi
 
 # Prepare attachments (Rails public/system) directory
-coloredEcho "Checking /app/public/system mount..." cyan
+echo "Checking /app/public/system mount..."
 if [[ ! -d /app/public/system ]]; then
-  coloredEcho "WARNING: /app/public/system is not mounted." red
+  echo "WARNING: /app/public/system is not mounted."
 else
   if [[ `stat -f -L -c %T /app/public/system` == "nfs" ]]; then
-    coloredEcho "WARNING: /app/public/system is an NFS directory. It may not be writable." red
+    echo "WARNING: /app/public/system is an NFS directory. It may not be writable."
   else
     chown -R jibjob:jibjob /app/public/system
-    coloredEcho "/app/public/system is mounted." green
+    echo "/app/public/system is mounted."
   fi
 fi
 
 # Configure nginx
-coloredEcho "Configuring nginx with RAILS_ENV=${RAILS_ENV}..." cyan
+echo "Configuring nginx with RAILS_ENV=${RAILS_ENV}..."
 erb /config/nginx.conf.erb > /opt/nginx/conf/nginx.conf
-coloredEcho "Written config to /opt/ntinx/conf/nginx.conf" green
+echo "Written config to /opt/ntinx/conf/nginx.conf"
 
 # Invoke mode program
 echo
 case $PROGRAM in
   console)
-    coloredEcho "Starting jibjob shell..." green
+    echo "Starting jibjob shell..."
     exec gosu jibjob /bin/bash --login
     ;;
   server)
     prepare_fonts
     echo
-    coloredEcho "Starting nginx..." green
+    echo "Starting nginx..."
     exec /opt/nginx/sbin/nginx
     ;;
   worker)
     prepare_fonts
     echo
-    coloredEcho "Running sidekiq..." green
+    echo "Running sidekiq..."
     cd /app
     exec gosu jibjob bin/sidekiq ${PROGRAM_ARGS}
     ;;
   root)
-    coloredEcho "Starting root shell..." green
+    echo "Starting root shell..."
     exec bash -l
     ;;
   *)
-    coloredEcho "Unknown mode...doing nothing." yellow
+    echo "Unknown mode...doing nothing."
     exit 1
     ;;
 esac
