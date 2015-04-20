73b8e8e6c67820d417556fb4bd6ac7e92a63a397
Virgil Dimaguila <virgild@gmail.com>
2015-04-20 18:27:01 -0400

    Refactor initializer. Consolidate mount checks at the beginning.

--- Docker/containers/app/initializer.sh
+++ Docker/containers/app/initializer.sh
@@ -4,31 +4,10 @@ set -e
 export PATH=${RUBY_PATH}:${PATH}
 export RAILS_ENV=${RAILS_ENV:-development}
 
-HAS_APP=0
 PROGRAM=$1
 PROGRAM_ARGS=${*:2:500}
 
-has_missing_config_files=0
-
-# Check app directory
-printf "Checking /app directory..."
-if [[ -d /app ]]; then
-  if [[ -e /app/bin/rails ]]; then
-    HAS_APP=1
-    printf "Found Rails app in /app.\n"
-  else
-    printf "Cannot find Rails app in /app.\n"
-    exit 1
-  fi
-else
-  printf "Cannot find /app. Mount the Rails root to /app in this container.\n"
-  exit 1
-fi
-
-# Prepare app
-if [[ $RAILS_ENV != "development" ]]; then
-  chown -R jibjob:jibjob /app
-fi
+MOUNTS_PASSING=1
 
 # Initialize fonts
 function prepare_fonts()
@@ -39,100 +18,89 @@ function prepare_fonts()
   printf "done.\n"
 }
 
-function check_config_files()
+function check_mount()
 {
-  echo "Checking /app/config/ files..."
-  printf " - database.yml..."
-  if [[ -e /app/config/database.yml ]]; then
+  printf "Checking mount: $1..."
+  if [[ -e $1 ]]; then
     printf "OK\n"
   else
-    has_missing_config_files=1
     printf "NOT FOUND\n"
-  fi
-
-  printf " - mailer.yml..."
-  if [[ -e /app/config/mailer.yml ]]; then
-    printf "OK\n"
-  else
-    has_missing_config_files=1
-    printf "NOT FOUND\n"
+    MOUNTS_PASSING=0
   fi
 }
 
-# Check files and directories
-check_config_files
+function main()
+{
+  # Check mounts
+  check_mount "/app"
+  check_mount "/app/public/system"
+  check_mount "/app/config/database.yml"
+  check_mount "/app/config/mailer.yml"
+  check_mount "/vendor/bundle"
 
-# Prepare gems directory
-printf "Checking /vendor/bundle directory..."
-if [[ ! -d /vendor/bundle ]]; then
-  printf "ERROR: /vendor/bundle not found."
-  exit 1
-fi
-chown -R jibjob:jibjob /vendor
-printf "done.\n"
+  if [[ $MOUNTS_PASSING -eq 0 ]]; then
+    echo "ERROR: There are missing mounts"
+    exit 1
+  fi
 
-# Run bundler
-printf "Running bundler..."
-gosu jibjob touch /vendor/BUNDLER_RUNNING
-cd /app
-gosu jibjob bundle install --path=/vendor/bundle --no-cache --shebang=${RUBY_PATH}/ruby &> /vendor/bundle/install.log
-gosu jibjob rm -f /vendor/BUNDLER_RUNNING
-printf "finished bundler install.\n"
+  # Prepare app
+  if [[ $RAILS_ENV != "development" ]]; then
+    chown -R jibjob:jibjob /app
+  fi
 
-# Prepare attachments (Rails public/system) directory
-echo "Checking /app/public/system mount..."
-if [[ ! -d /app/public/system ]]; then
-  echo " - ERROR: /app/public/system is not mounted."
-  exit 1
-else
+  # Chown the chown
+  chown -R jibjob:jibjob /vendor
+
+  # Handle attachments directory special cases
   if [[ `stat -f -L -c %T /app/public/system` == "nfs" ]]; then
-    echo " - WARNING: /app/public/system is an NFS directory. It may not be writable."
+    echo " - WARNING: /app/public/system is an NFS directory."
   else
     chown -R jibjob:jibjob /app/public/system
-    echo " - /app/public/system is mounted."
   fi
-fi
 
-# Configure nginx
-echo "Configuring nginx with RAILS_ENV=${RAILS_ENV}..."
-erb /config/nginx.conf.erb > /opt/nginx/conf/nginx.conf
-echo " - Written config to /opt/ntinx/conf/nginx.conf"
+  # Run bundler
+  printf "Running bundler..."
+  gosu jibjob touch /vendor/BUNDLER_RUNNING
+  cd /app
+  if [[ $RAILS_ENV == "development" ]]; then
+    gosu jibjob bundle install --path=/vendor/bundle --no-cache --shebang=${RUBY_PATH}/ruby &> /vendor/bundle/install.log
+  else
+    gosu jibjob bundle install --path=/vendor/bundle --deployment --without="development test" --no-cache --shebang=${RUBY_PATH}/ruby &> /vendor/bundle/install.log
+  fi
+  gosu jibjob rm -f /vendor/BUNDLER_RUNNING
+  printf "OK\n"
 
-if [[ $has_missing_config_files -eq 1 ]]; then
-  echo
-  echo "-----------------------------------------"
-  echo "WARNING - There are missing config files."
-  echo "-----------------------------------------"
-fi
+  # Configure nginx
+  printf "Configuring nginx with RAILS_ENV=${RAILS_ENV}..."
+  erb /config/nginx.conf.erb > /opt/nginx/conf/nginx.conf
+  printf "OK\n"
 
-# Invoke mode program
-case $PROGRAM in
-  console)
-    echo
-    echo "Starting jibjob shell..."
-    exec gosu jibjob /bin/bash --login
-    ;;
-  server)
-    prepare_fonts
-    echo
-    echo "Starting nginx..."
-    exec /opt/nginx/sbin/nginx
-    ;;
-  worker)
-    prepare_fonts
-    echo
-    echo "Running sidekiq..."
-    cd /app
-    exec gosu jibjob bin/sidekiq ${PROGRAM_ARGS}
-    ;;
-  root)
-    echo
-    echo "Starting root shell..."
-    exec bash -l
-    ;;
-  *)
-    echo
-    echo "Unknown mode...doing nothing."
-    exit 1
-    ;;
-esac
+  # Invoke mode program
+  case $PROGRAM in
+    console)
+      echo
+      echo "Starting jibjob shell..."
+      exec gosu jibjob /bin/bash --login
+      ;;
+    server)
+      prepare_fonts
+      echo
+      echo "Starting nginx..."
+      exec /opt/nginx/sbin/nginx
+      ;;
+    worker)
+      prepare_fonts
+      echo
+      echo "Running sidekiq..."
+      cd /app
+      exec gosu jibjob bin/sidekiq ${PROGRAM_ARGS}
+      ;;
+    *)
+      echo
+      echo "Unknown mode...doing nothing. (Modes are: console | server | worker)"
+      exit 1
+      ;;
+  esac
+}
+
+main
