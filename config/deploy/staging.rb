ab85795e8a3be7d4b5b6e0a8b6c8408b804b6792
Virgil Dimaguila <virgild@gmail.com>
2015-04-26 14:47:32 -0400

    Staging workers.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,7 +7,7 @@ set :default_env, {
 
 server ENV['STAGING_HOST'], primary: true, roles: ['app', 'worker', 'db', 'web']
 
-set :sidekiq_options_per_process, ["-c 10", "--queue default", "--queue mailers", "--queue logging"]
+set :sidekiq_options_per_process, ["-c 10 --queue default --queue mailers --queue logging"]
 
 set :ssh_options, {
   forward_agent: true,
