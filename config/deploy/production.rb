539a7e5099fe954c6f235b3ebd5126588f0436f2
Virgil Dimaguila <virgild@gmail.com>
2015-04-26 22:12:21 -0400

    Deploy updates.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,49 +1,15 @@
-# Simple Role Syntax
-# ==================
-# Supports bulk-adding hosts to roles, the primary server in each group
-# is considered to be the first unless any hosts have the primary
-# property set.  Don't declare `role :all`, it's a meta role.
+set :deploy_to, ENV['DEPLOY_DIRECTORY']
+set :user, ENV['DEPLOY_APP_USER']
 
-role :app, [ENV['DEPLOY_APP_SERVER']]
-role :web, [ENV['DEPLOY_APP_SERVER']]
-role :db,  [ENV['DEPLOY_APP_SERVER']]
-role :worker, [ENV['DEPLOY_APP_SERVER']]
-
-# Extended Server Syntax
-# ======================
-# This can be used to drop a more detailed server definition into the
-# server list. The second argument is a, or duck-types, Hash and is
-# used to set extended properties on the server.
+set :default_env, {
+  'PATH' => ENV['DEPLOY_ENV_PATH']
+}
 
 server ENV['DEPLOY_APP_SERVER'], user: ENV['DEPLOY_APP_USER'], roles: %w{web app db worker}
 
+set :sidekiq_options_per_process, ["-c 20 --queue default --queue mailers --queue logging"]
 
 set :ssh_options, {
-  forward_agent: true
+  forward_agent: true,
+  user: ENV['DEPLOY_APP_USER']
 }
-
-# Custom SSH Options
-# ==================
-# You may pass any option but keep in mind that net/ssh understands a
-# limited set of options, consult[net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start).
-#
-# Global options
-# --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-#
-# And/or per server (overrides global)
-# ------------------------------------
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
